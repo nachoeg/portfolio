1ae7345190fad4282dd1adecf57a00aed5168857
nachoeg <nachoeg99@gmail.com>
2024-04-02 05:30:53 -0300

    add media query hover-hover

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,17 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
+    screens: {
+      xs: "375px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       screens: {
-        xs: "375px",
+        "hover-hover": { raw: "(hover: hover)" },
       },
     },
   },
