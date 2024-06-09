467e38f2426c8ba8142097b281839515386207e2
nachoeg <nachoeg99@gmail.com>
2024-06-09 18:58:54 -0300

    update stack.js

--- src/data/stack.js
+++ src/data/stack.js
@@ -2,18 +2,24 @@ import TAGS from "./tags";
 
 const STACK = {
   primary: [
-    TAGS.NEXTJS,
     TAGS.REACT,
     TAGS.ASTRO,
-    TAGS.TYPESCRIPT,
-    TAGS.SPRING,
     TAGS.JAVA,
     TAGS.JS,
     TAGS.TAILWIND,
     TAGS.CSS,
     TAGS.HTML,
   ],
-  secondary: [TAGS.EXPRESS, TAGS.MYSQL, TAGS.DOCKER, TAGS.BOOTSTRAP, TAGS.MUI],
+  secondary: [
+    TAGS.NEXTJS,
+    TAGS.SPRING,
+    TAGS.TYPESCRIPT,
+    TAGS.EXPRESS,
+    TAGS.MYSQL,
+    TAGS.DOCKER,
+    TAGS.BOOTSTRAP,
+    TAGS.MUI,
+  ],
 };
 
 export default STACK;
