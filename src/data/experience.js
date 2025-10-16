6348f0c0315ad53e6637ab47555dd39d2bf66c11
iescudero <iescudero@cespi.unlp.edu.ar>
2025-10-16 03:31:18 -0300

    update navbar, add experience, rearrange pages

---
+++ src/data/experience.js
@@ -0,0 +1,18 @@
+import TAGS from "./tags";
+
+export const EXPERIENCE = [
+  {
+    id: 1,
+    image: "/assets/cespi.webp",
+    company: "CeSPI",
+    title: "Desarrollador Java Backend/Frontend Web Jr.",
+    description:
+      "Trabaje en el desarrollo y mantenimiento de SEM (Solución de Estacionamiento Medido), una aplicación web para la gestión de estacionamientos medidos utilizada en todo el país. Implemente nuevas funcionalidades, desde el frontend con Angular y Bootstrap, hasta el backend con Spring Boot y Java. Además, hice migraciones en la base de datos MySQL.",
+    location: "La Plata, Argentina",
+    mode: "Híbrido",
+    url: "https://www.cespi.unlp.edu.ar/",
+    startDate: "Marzo 2025",
+    endDate: "Actualidad",
+    tags: [TAGS.SPRING, TAGS.JAVA, TAGS.MYSQL, TAGS.ANGULAR, TAGS.BOOTSTRAP],
+  },
+];
