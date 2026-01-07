d37a535bf4042f8daeb917e614f9a10a2134a590
iescudero <iescudero@cespi.unlp.edu.ar>
2026-01-07 18:45:31 -0300

    update footer and spacing

---
+++ src/data/social_links.js
@@ -0,0 +1,6 @@
+export const SOCIAL_LINKS = {
+  MAIL: "mailto:ignacioescuderogonzalez@gmail.com",
+  CV: "https://docs.google.com/document/d/1WqOOm3WlPbsPD2ydXGd1ve4-GenKoidLuSngW2gS5LA/edit?usp=sharing",
+  LINKEDIN: "https://www.linkedin.com/in/ignacio-escudero-gonzalez/",
+  GITHUB: "https://github.com/nachoeg",
+};
